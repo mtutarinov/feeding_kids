83e178e6eef1361e5b64cdea27135689ddec6683
mtutarinov <mihailtutarinov2@gmail.com>
2024-12-17 16:32:38 +0300

    add constructor

--- cart/cart.py
+++ cart/cart.py
@@ -14,26 +14,18 @@ class Cart:
         self.cart = cart
 
     def add(self, ingredient_id):
-
+        """Добавляет ингредиент в корзину."""
         if ingredient_id not in self.cart:
             self.cart.append(ingredient_id)
         self.save()
 
     def save(self):
+        """Сохраняет сессию."""
         self.session.modified = True
 
-    # def remove(self, painting):
-    #     painting_id = str(painting.id)
-    #     if painting_id in self.cart:
-    #         del self.cart[painting_id]
-    #         self.save()
-    #
-    # def __iter__(self):
-    #     painting_ids = self.cart.keys()
-    #     paintings = Painting.objects.filter(id__in=painting_ids)
-    #     cart = self.cart.copy()
-    #     for painting in paintings:
-    #         cart[str(painting.id)]['painting'] = painting
-    #     for item in cart.values():
-    #         item['price'] = int(item['price'])
-    #         yield itemя
+    def remove(self, ingredient_id):
+        """Удаляет ингредиент из корзины. """
+        self.cart.remove(ingredient_id)
+        self.save()
+
+
